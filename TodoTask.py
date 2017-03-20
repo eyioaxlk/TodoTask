15342a6f830591c0fbeb4367b7cec2ccbe0bb8d9
eyioaxlk <eyioax@gmail.com>
2017-03-20 16:21:20 +0800

    修改代码，添加排版和输入框

--- TodoTask.py
+++ TodoTask.py
@@ -5,8 +5,11 @@ __author__ = 'yexiao'
 
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow,
-                             QGridLayout, QPushButton, QMessageBox)
+                             QGridLayout, QPushButton, QMessageBox,
+                             QHBoxLayout, QVBoxLayout, QLineEdit,
+                             QTextEdit, QLabel)
 from PyQt5.QtCore import QCoreApplication
+
 
 class TodoTask(QWidget):
 
@@ -18,6 +21,10 @@ class TodoTask(QWidget):
 
     def initUI(self):
 
+        # 增加一个状态栏
+        # self.statusBar()
+
+        """
         # 创建关闭按钮
         closebtn = QPushButton('关闭', self)
         closebtn.clicked.connect(QCoreApplication.instance().quit)
@@ -26,7 +33,45 @@ class TodoTask(QWidget):
         # 创建确认按钮
         enterbtn = QPushButton('确定', self)
         enterbtn.move(200, 250)
+        """
 
+        closebtn = QPushButton('Close', self)
+        enterbtn = QPushButton('Enter', self)
+
+        title = QLabel('填写任务')
+
+        titleEditename = QLabel('主题')
+        titleEdit = QLineEdit()
+        ttbox = QHBoxLayout()
+        ttbox.addWidget(titleEditename)
+        ttbox.addWidget(titleEdit)
+
+        eebox = QHBoxLayout()
+        egEditname = QLabel('简介')
+        egEdit = QLineEdit()
+        eebox.addWidget(egEditname)
+        eebox.addWidget(egEdit)
+
+        cebox = QHBoxLayout()
+        contentEditname = QLabel('内容')
+        contentEdit = QTextEdit()
+        cebox.addWidget(contentEditname)
+        cebox.addWidget(contentEdit)
+
+        btbox = QHBoxLayout()
+        btbox.addStretch(1)
+        btbox.addWidget(closebtn)
+        btbox.addWidget(enterbtn)
+
+        vbox = QVBoxLayout()
+        # vbox.addStretch(1)
+        vbox.addWidget(title)
+        vbox.addLayout(ttbox)
+        vbox.addLayout(eebox)
+        vbox.addLayout(cebox)
+        vbox.addLayout(btbox)
+
+        self.setLayout(vbox)
 
         # 主窗口状态
         self.setGeometry(300, 300, 300, 300)
